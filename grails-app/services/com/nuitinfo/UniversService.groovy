ce004482fce100304b2172446fafddd5b0085941
kazamafury <thierry.weissbeck@gmail.com>
2012-12-07 00:46:33 +0100

    git add Univers Service

---
+++ grails-app/services/com/nuitinfo/UniversService.groovy
@@ -0,0 +1,8 @@
+package com.nuitinfo
+
+class UniversService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ test/unit/com/nuitinfo/UniversServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.nuitinfo
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(UniversService)
+class UniversServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
