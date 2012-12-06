d34da58a511fed2295c8b1408be37425d1a8ff75
kazamafury <thierry.weissbeck@gmail.com>
2012-12-06 22:36:37 +0100

    add domains

---
+++ test/unit/com/nuitinfo/CategorieTests.groovy
@@ -0,0 +1,17 @@
+package com.nuitinfo
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Categorie)
+class CategorieTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
