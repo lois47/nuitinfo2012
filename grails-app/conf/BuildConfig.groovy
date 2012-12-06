40c342c773f6b83db74e26b879588a22bfbfa7f5
kazamafury <thierry.weissbeck@gmail.com>
2012-12-06 17:19:22 +0100

    add cloud foundry conf and pro database conf

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -52,6 +52,10 @@ grails.project.dependency.resolution = {
 
         runtime ":database-migration:1.1"
 
+		compile ":spring-security-core:1.2.7.3"
+		compile ":mail:1.0.1"
+		compile ":cloud-foundry:1.2.3"
+		compile ":webxml:1.4.1"
         compile ':cache:1.0.0'
     }
 }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,19 +30,12 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+             driverClassName = "org.postgresql.Driver"
+			username = "postgres"
+			password = "admin"
+			
+			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:postgresql://localhost:5432/nuitinfoBD"
         }
     }
 }
