7ad2c26058dfabc6b7058b9424f18161efc85cea
Autar <vincentcarassus@gmail.com>
2012-12-07 04:06:42 +0100

    Redirections sur controlleurs Univers et Categories

--- grails-app/controllers/com/nuitinfo/CategorieController.groovy
+++ grails-app/controllers/com/nuitinfo/CategorieController.groovy
@@ -5,10 +5,12 @@ class CategorieController {
     def listePatrimoines(Long id) {
 		if(!id) {
 			redirect(uri:'/')
+			return
 		}
 		Categorie categorie = Categorie.findById(id)
 		if(!categorie) {
 			redirect(uri:'/')
+			return
 		}
 		[categorie: categorie]
 	}
--- grails-app/controllers/com/nuitinfo/UniversController.groovy
+++ grails-app/controllers/com/nuitinfo/UniversController.groovy
@@ -5,10 +5,12 @@ class UniversController {
     def listeCategories(Long id) {
 		if(!id) {
 			redirect(uri:'/')
+			return
 		}
 		Univers universInstance = Univers.findById(id)
 		if(!universInstance) {
 			redirect(uri:'/')
+			return
 		}
 		[universInstance: universInstance]
 	}
