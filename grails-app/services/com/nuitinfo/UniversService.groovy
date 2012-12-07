5e7d716d38bf861a9da8b4b69eef91aeb96749bc
kazamafury <thierry.weissbeck@gmail.com>
2012-12-07 01:52:14 +0100

    add service univers

--- grails-app/services/com/nuitinfo/UniversService.groovy
+++ grails-app/services/com/nuitinfo/UniversService.groovy
@@ -5,4 +5,12 @@ class UniversService {
     def serviceMethod() {
 
     }
+	
+	def Categorie[] getBestCategrories(id) {
+		Univers univers = Univers.findById(id);
+		for (Categorie cat : univers.categories) {
+			
+		}
+		
+	}
 }
